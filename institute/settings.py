616185cf41eb1c19a7fb2c50b019b9b008bff50d
Jonathan Murmu <jonathanmurmu@gmail.com>
2018-08-05 23:17:53 +0530

    addded dot env settings

--- institute/settings.py
+++ institute/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(dotenv_path=BASE_DIR+'/institute/.env')
 
 
 # Quick-start development settings - unsuitable for production
