5133742766b2792afbdb43e925afc8e460c6ecd0
Jonathan Murmu <jonathanmurmu@gmail.com>
2018-08-05 22:44:47 +0530

    changed urls

--- institute/urls.py
+++ institute/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('course/', include('course.urls')),
+    path('edu/api-auth/', include('rest_framework.urls')),
+    path('edu/admin/', admin.site.urls),
+    path('edu/accounts/', include('accounts.urls')),
+    path('edu/course/', include('course.urls')),
 ]
