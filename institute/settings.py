a670367aa9559124fe052c7958ab029a8f81530a
Jonathan Murmu <jonathanmurmu@gmail.com>
2018-08-05 23:41:05 +0530

    added allowed host

--- institute/settings.py
+++ institute/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-127-237-253.ap-south-1.compute.amazonaws.com',
+                 'localhost']
 
 
 # Application definition
